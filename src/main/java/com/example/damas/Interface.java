0d02fa7cc50902fca3e5fd307d2eee976313f7ca
talbertosilva <100167282+talbertosilva@users.noreply.github.com>
2022-06-04 15:15:08 +0100

    Interface - Get ip address

--- src/main/java/com/example/damas/Interface.java
+++ src/main/java/com/example/damas/Interface.java
@@ -1,4 +1,27 @@
 package com.example.damas;
 
-public class Interface {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ResourceBundle;
+
+public class Interface implements Initializable {
+
+    @FXML
+    Text txt_ip, txt_porta;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            txt_ip.setText(ip.getHostAddress());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        txt_porta.setVisible(false);
+    }
 }
