7fcfd2b029cc3fb03e7696c40630519574696a1f
Gaspar <gasparvilasespinheira@gmail.com>
2022-06-11 15:52:21 +0100

    Página Instruções 0.2

--- src/main/java/com/example/damas/Menu.java
+++ src/main/java/com/example/damas/Menu.java
@@ -37,4 +37,12 @@ public class Menu {
     {
         System.exit(0);
     }
+
+    public void menuInstrucoes(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("instrucoes.fxml"));
+        stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
